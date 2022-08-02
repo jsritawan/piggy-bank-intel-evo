8b22faa0663db5e0e6819bf1ee2cf53c23d14d87
jeerapat <jeerapat@mfec.co.th>
2022-08-02 23:54:44 +0700

    list wallet and select wallet

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import categorySlice from "../features/category/category-slice";
 import txnSlice from "../features/transactions/transactions-slice";
+import walletsSlice from "../features/wallets/wallets-slice";
 
 export const store = configureStore({
   reducer: {
     transactions: txnSlice,
     categories: categorySlice,
+    walletState: walletsSlice,
   },
 });
 
---
+++ src/features/wallets/wallets-slice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+export interface IWallet {
+  id: string;
+  userId: string;
+  balance: number;
+  name: string;
+  createAt: string;
+  updateAt?: string;
+}
+
+export interface IWalletState {
+  selectedWallet?: IWallet;
+  wallets: IWallet[];
+}
+const initialState: IWalletState = {
+  wallets: [],
+};
+
+const walletsSlice = createSlice({
+  name: "wallets",
+  initialState,
+  reducers: {
+    addWallet: function (state, action: PayloadAction<IWallet>) {
+      state.wallets.push(action.payload);
+    },
+    selectWallet: function (state, action: PayloadAction<string>) {
+      const { wallets } = state;
+      const { payload: walletId } = action;
+      state.selectedWallet = wallets.find((w) => w.id === walletId);
+    },
+  },
+});
+
+export const { addWallet, selectWallet } = walletsSlice.actions;
+export default walletsSlice.reducer;
